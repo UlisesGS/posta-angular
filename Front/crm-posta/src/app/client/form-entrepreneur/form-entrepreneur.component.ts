e9bd62e7a371f766a9a8b098534235eff6d508f7
Ulises Gadea <ulisesgadea28@gmail.com>
2023-06-17 21:52:29 -0300

    agregando businessman

--- Front/crm-posta/src/app/client/form-entrepreneur/form-entrepreneur.component.ts
+++ Front/crm-posta/src/app/client/form-entrepreneur/form-entrepreneur.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Entrepreneur } from '../entrepreneur';
+import { ClientService } from '../client.service';
+import { Router } from '@angular/router';
+import { Municipio } from 'src/app/municipio/municipio';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-entrepreneur',
@@ -7,14 +11,26 @@ import { Entrepreneur } from '../entrepreneur';
   styleUrls: ['./form-entrepreneur.component.css']
 })
 export class FormEntrepreneurComponent implements OnInit {
+  constructor(private clientService:ClientService,private router:Router){}
   emprendedor:Entrepreneur= new Entrepreneur();
-  constructor(){}
+  municipios:Municipio[]=[];
+
   ngOnInit(): void {
-
+    this.clientService.getClientsMunicipios().subscribe(data=>{
+      this.municipios=data;
+      console.log(data);
+    
+    })
   }
-
 public registrar(){
   console.log(this.emprendedor);
+  this.clientService.saveEntrepreneur(this.emprendedor).subscribe(data=>{
+    Swal.fire(`Creado`, `Emprendedor ${data.name} fue creado con exito`, `success`)
+    this.router.navigate[('/clients')]
+  },e=>{
+    
+
+  })
 
 }
 
