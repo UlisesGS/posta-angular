7260b18e157bb6487cc73798ddf45f886de5327d
damian <adamodamian6@gmail.com>
2023-06-21 20:19:59 -0300

    editar usuario terminado

--- Front/crm-posta/src/app/usuario/usuarioform/usuarioform.component.ts
+++ Front/crm-posta/src/app/usuario/usuarioform/usuarioform.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../usuario.service';
 import { Usuario } from '../usuario';
 import Swal from 'sweetalert2';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuarioform',
@@ -13,22 +13,45 @@ export class UsuarioformComponent implements OnInit {
   usuario: Usuario = new Usuario();
   public titulo: string = 'Formulario Usuario'
   ngOnInit(): void {
+    this.rutaParametro.paramMap.subscribe(parametro=>{
+      let id=+parametro.get('id');
+     if(id){
+      this.usuarioService.usuarioFindById(id).subscribe(data=>{
+        this.usuario=data;
+        this.titulo='Editar Usuario';
+      })
+     }else{
+      this.titulo='Crear Usuario';
+     }
+
+    })
 
   }
-  constructor(private usuarioService: UsuarioService, private ruta: Router) {
+  constructor(private usuarioService: UsuarioService, private ruta: Router,private rutaParametro:ActivatedRoute) {
 
   }
   public registrar() {
     console.log(this.usuario);
     this.usuarioService.usuarioSave(this.usuario).subscribe(data => {
       Swal.fire('Creado', `El usuario ${data.name} fue creado con exito`, 'success');
-      this.ruta.navigate[('/usuarios')]
+      this.ruta.navigate(['/usuarios'])
 
     }, e => {
       Swal.fire('Error: ', 'Ingrese bien los datos', 'error');
     })
 
   }
+public editar(){
+ // Swal.fire('En construccion', 'Esta seccion esta en construccion', 'info');
+ this.usuarioService.usuarioUpdate(this.usuario).subscribe(data=>{
+  this.ruta.navigate(['/usuarios'])
+  Swal.fire('Editado', `El usuario ${data.name} fue editado con exito`, 'success');
 
+ },e=>{
+  console.log(e);
+
+  Swal.fire('Error: ', 'Ingrese bien los datos', 'error');
+ })
+}
 
 }
