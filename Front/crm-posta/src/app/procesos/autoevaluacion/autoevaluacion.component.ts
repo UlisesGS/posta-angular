4ed6e0f57a64203b31bb345b5065cd94f4ccec96
damian <adamodamian6@gmail.com>
2023-06-29 21:00:59 -0300

    realizando la autoevaluacion de cliente funcional

--- Front/crm-posta/src/app/procesos/autoevaluacion/autoevaluacion.component.ts
+++ Front/crm-posta/src/app/procesos/autoevaluacion/autoevaluacion.component.ts
@@ -4,6 +4,8 @@ import { Client } from 'src/app/client/client';
 import { ClientService } from 'src/app/client/client.service';
 import { ModalService } from 'src/app/client/modal.service';
 import { ProcesoService } from '../proceso.service';
+import { Canvas } from '../canvas';
+import { SelfAssessment } from './../selfAssessment';
 
 @Component({
   selector: 'app-autoevaluacion',
@@ -11,6 +13,9 @@ import { ProcesoService } from '../proceso.service';
   styleUrls: ['./autoevaluacion.component.css']
 })
 export class AutoevaluacionComponent implements OnInit {
+  selfAssessment:SelfAssessment= new SelfAssessment();
+  preguntas:any[]=[]
+  canvas:Canvas= new Canvas();
   cliente: Client = new Client()
   constructor(private modalService: ModalService,
      private clienteService: ClientService,
@@ -18,22 +23,43 @@ export class AutoevaluacionComponent implements OnInit {
      private canvasService:ProcesoService,
      ) { }
   ngOnInit(): void {
+
+
+    this.canvasService.canvasSave(this.canvas).subscribe(data=>{
+    //  console.log('canvas'+data);
+
+      this.canvas=data;
+
+    })
     this.rutaParametro.paramMap.subscribe(parametro => {
       let id = +parametro.get('id');
       if (id) {
-        this.clienteService.getClient(id).subscribe(data => {
-          this.cliente = data;
-          console.log(data);
+        this.clienteService.getClient(id).subscribe(d => {
+          this.cliente = d;
+          console.log(d);
+          this.cliente.canvas=this.canvas;
+          this.clienteService.updateBusinessman(this.cliente).subscribe(c=>{
+            this.cliente=c;
+          })
+         // console.log(data);
 
         })
       }
     })
+    //this.cliente.canvas=this.canvas;
+    console.log(this.cliente);
+/*
+
+    //console.log(this.cliente);
+*/
   }
 
   cerrarModalAsesoria(): void {
     this.modalService.cerrarModalAsesoria();
   }
   public guardar(){
+console.log(this.preguntas);
+
 
   }
 
