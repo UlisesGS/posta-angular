bccec05c3bb04f12d6bc2170e67bbdfb0f37e945
Ulises Gadea <ulisesgadea28@gmail.com>
2023-07-05 20:39:53 -0300

    agregando

--- Front/crm-posta/src/app/procesos/ver-procesos/canales-modal/canales-modal.component.ts
+++ Front/crm-posta/src/app/procesos/ver-procesos/canales-modal/canales-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Process } from '../../Process';
 import { ModalService } from 'src/app/client/modal.service';
+import { ProcesoService } from '../../proceso.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-canales-modal',
@@ -9,8 +11,10 @@ import { ModalService } from 'src/app/client/modal.service';
 })
 export class CanalesModalComponent {
   @Input()proceso:Process= new Process();
+  @Input()bool:boolean;
   constructor(
     private modalServide:ModalService,
+    private procesoService:ProcesoService
   ){}
 
   ngOnInit(): void {
@@ -20,5 +24,12 @@ export class CanalesModalComponent {
 
   public cerrarVerMas(){
     this.modalServide.cerrarVerMas2();
+  }
+
+  public editar(){
+    this.procesoService.canalesPut(this.proceso.canvasModel.channels).subscribe(dato=>{
+    this.modalServide.cerrarVerMas2();
+    Swal.fire('Editado ', 'Los canales fueron editados con exito', 'success');
+    })
   }
 }
