04d70462c58016afd2a260ebff62263beb05769c
damian <adamodamian6@gmail.com>
2023-07-06 18:47:16 -0300

    editar hasta actividades claves

--- Front/crm-posta/src/app/procesos/ver-procesos/actividades-claves-modal/actividades-claves-modal.component.ts
+++ Front/crm-posta/src/app/procesos/ver-procesos/actividades-claves-modal/actividades-claves-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Process } from '../../Process';
 import { ModalService } from 'src/app/client/modal.service';
+import { ProcesoService } from '../../proceso.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-actividades-claves-modal',
@@ -11,17 +13,23 @@ export class ActividadesClavesModalComponent {
   @Input()proceso:Process= new Process();
   @Input()bool:boolean;
   constructor(
-    private modalServide:ModalService,
+    private modalServide:ModalService,private procesoService:ProcesoService
   ){}
 
   ngOnInit(): void {
     console.log(this.proceso);
     console.log(this.bool);
-    
+
   }
 
   public cerrarVerMas(){
     this.modalServide.cerrarVerMas5();
+  }
+  public editar(){
+    this.procesoService.actividadesClavesPut(this.proceso.canvasModel.keyActivities).subscribe(dato=>{
+    this.modalServide.cerrarVerMas5();
+    Swal.fire('Editado ', 'La Actividades Claves fue editada con exito', 'success');
+    })
   }
 }
 
--- Front/crm-posta/src/app/procesos/ver-procesos/recursos-claves-modal/recursos-claves-modal.component.ts
+++ Front/crm-posta/src/app/procesos/ver-procesos/recursos-claves-modal/recursos-claves-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Process } from '../../Process';
 import { ModalService } from 'src/app/client/modal.service';
+import { ProcesoService } from '../../proceso.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-recursos-claves-modal',
@@ -9,17 +11,26 @@ import { ModalService } from 'src/app/client/modal.service';
 })
 export class RecursosClavesModalComponent {
   @Input()proceso:Process= new Process();
+  @Input()bool:boolean;
   constructor(
-    private modalServide:ModalService,
+    private modalServide:ModalService,private procesoService:ProcesoService
   ){}
 
   ngOnInit(): void {
     console.log(this.proceso);
-    
+    console.log(this.bool);
+
+
   }
 
   public cerrarVerMas(){
     this.modalServide.cerrarVerMas4();
+  }
+  public editar(){
+    this.procesoService.recursosClavesPut(this.proceso.canvasModel.keyRecources).subscribe(dato=>{
+    this.modalServide.cerrarVerMas4();
+    Swal.fire('Editado ', 'Recursos Claves fue editado con exito', 'success');
+    })
   }
 }
 
