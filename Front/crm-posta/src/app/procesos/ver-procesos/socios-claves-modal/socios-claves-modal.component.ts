de453172e6a7b579fe7133cfea5047024df83f5b
damian <adamodamian6@gmail.com>
2023-07-06 19:00:23 -0300

    socios claves editar

--- Front/crm-posta/src/app/procesos/ver-procesos/socios-claves-modal/socios-claves-modal.component.ts
+++ Front/crm-posta/src/app/procesos/ver-procesos/socios-claves-modal/socios-claves-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Process } from '../../Process';
 import { ModalService } from 'src/app/client/modal.service';
+import { ProcesoService } from '../../proceso.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-socios-claves-modal',
@@ -9,16 +11,23 @@ import { ModalService } from 'src/app/client/modal.service';
 })
 export class SociosClavesModalComponent {
   @Input()proceso:Process= new Process();
+  @Input()bool:boolean;
   constructor(
-    private modalServide:ModalService,
+    private modalServide:ModalService,private procesoService:ProcesoService
   ){}
 
   ngOnInit(): void {
     console.log(this.proceso);
-    
+
   }
 
   public cerrarVerMas(){
     this.modalServide.cerrarVerMas6();
+  }
+  public editar(){
+    this.procesoService.sociosClavesPut(this.proceso.canvasModel.keyPartners).subscribe(dato=>{
+    this.modalServide.cerrarVerMas6();
+    Swal.fire('Editado ', 'Socios Claves fue editado con exito', 'success');
+    })
   }
 }
