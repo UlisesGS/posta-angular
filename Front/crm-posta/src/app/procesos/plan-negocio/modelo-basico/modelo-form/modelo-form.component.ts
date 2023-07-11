f3f662306f8c12a06dc390d78c151ca04f0bafea
Ulises Gadea <ulisesgadea28@gmail.com>
2023-07-11 20:32:50 -0300

    hola

---
+++ Front/crm-posta/src/app/procesos/plan-negocio/modelo-basico/modelo-form/modelo-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modelo-form',
+  templateUrl: './modelo-form.component.html',
+  styleUrls: ['./modelo-form.component.css']
+})
+export class ModeloFormComponent {
+
+}
