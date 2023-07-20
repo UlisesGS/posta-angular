585141cea5e5061780d0e831734090d7ff4ac930
damian <adamodamian6@gmail.com>
2023-07-20 17:25:57 -0300

    ipc

--- Front/crm-posta/src/app/procesos/plan-negocio/plan-financiero/BusinessPlanFinancial.ts
+++ Front/crm-posta/src/app/procesos/plan-negocio/plan-financiero/BusinessPlanFinancial.ts
@@ -10,7 +10,20 @@ presupuestoCompra:PresupuestoCompra[];
 totalPresupuestoCompra:number=0.0;
 gastoCosto:GastoCosto;
 planInversion:PlanInversion;
-
+/*
+private Double IPC1;
+private Double IPC2;
+private Double IPC3;
+private Double IPC4;
+*/
+IPC1:number=0;
+IPC2:number=0;
+IPC3:number=0;
+IPC4:number=0;
+ipc1:number=0;
+ipc2:number=0;
+ipc3:number=0;
+ipc4:number=0;
 public cuentas(){
   this.presupuestoCompra.forEach(p=>{
     this.totalPresupuestoCompra+=p.totalAnual;
