2282455e98c7ccaac38607ad120e7c755c443f5a
Crowly <leandro.n.nunez@gmail.com>
2023-07-23 17:39:49 -0300

    Agregando metodos de cargar imagenes y ver imagenes

--- Front/crm-posta/src/app/client/detalles/detalles.component.ts
+++ Front/crm-posta/src/app/client/detalles/detalles.component.ts
@@ -5,6 +5,7 @@ import { ClientService } from '../client.service';
 import { Client } from '../client';
 import { Process } from 'src/app/procesos/Process';
 import { ModalService } from '../modal.service';
+import { ImagenService } from './imagen.service';
 
 @Component({
   selector: 'app-detalles',
@@ -29,8 +30,98 @@ export class DetallesComponent {
     private rutaParametro: ActivatedRoute,
     private clienteService: ClientService,
     public modalService: ModalService,
+    public imagenService:ImagenService,
   ) { }
 
+  file: File | null = null;
+  imageUrl: string | null = null;
+  imageUrlEncuesta: string | null = null;
+  imageUrlCierre: string | null = null;
+
+  onFileSelected(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  uploadImage() {
+    if (this.file) {
+      this.imagenService.uploadImage(this.file,this.proceso).subscribe(
+        (response) => {
+          console.log(response); // Maneja la respuesta del backend
+        },
+        (error) => {
+          console.error(error); // Maneja el error, si ocurre
+        }
+      );
+    }
+  }
+  getImageUrl() {
+    this.imagenService.getImageBlob(this.proceso).subscribe(
+      (blob) => {
+        this.imageUrl = URL.createObjectURL(blob);
+        console.log(this.imageUrl);
+        
+      },
+      (error) => {
+        console.error(error); // Maneja el error, si ocurre
+      }
+    );
+  }
+
+  uploadImageEncuesta() {
+    if (this.file) {
+      this.imagenService.uploadImageEncuesta(this.file,this.proceso).subscribe(
+        (response) => {
+          console.log(response); // Maneja la respuesta del backend
+        },
+        (error) => {
+          console.error(error); // Maneja el error, si ocurre
+        }
+      );
+    }
+  }
+  getImageUrlEncuesta() {
+    this.imagenService.getImageBlobEncuesta(this.proceso).subscribe(
+      (blob) => {
+        this.imageUrlEncuesta = URL.createObjectURL(blob);
+        console.log(this.imageUrl);
+        
+      },
+      (error) => {
+        console.error(error); // Maneja el error, si ocurre
+      }
+    );
+  }
+
+  uploadImageCierre() {
+    if (this.file) {
+      this.imagenService.uploadImageCierre(this.file,this.proceso).subscribe(
+        (response) => {
+          console.log(response); // Maneja la respuesta del backend
+        },
+        (error) => {
+          console.error(error); // Maneja el error, si ocurre
+        }
+      );
+    }
+  }
+  getImageUrlCierre() {
+    this.imagenService.getImageBlobCierre(this.proceso).subscribe(
+      (blob) => {
+        this.imageUrlCierre = URL.createObjectURL(blob);
+        console.log(this.imageUrl);
+        
+      },
+      (error) => {
+        console.error(error); // Maneja el error, si ocurre
+      }
+    );
+  }
+
+  // getImageUrl() {
+  //   console.log(this.imageUrl);
+    
+  //   this.imageUrl = this.imagenService.getImageUrl(this.proceso);
+  // }
 
   ngOnInit(): void {
     this.rutaParametro.paramMap.subscribe(parametro => {
@@ -65,19 +156,55 @@ export class DetallesComponent {
     this.negociosB=negociosB
     this.financieroB=financieroB
     this.procesoSeleccionado = proceso;
-
     console.log(this.autoB);
     console.log(this.canvasB);
     console.log(this.negociosB);
     console.log(this.financieroB);
-    
-    
-    
-    
-
     this.modalService.abrirTestAuto();
   }
 
- 
+  imprimirActa() {
+    const pdfUrl = 'assets/ActaCierreEmprendedor.pdf'; // Reemplaza con la ruta correcta a tu archivo PDF
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = pdfUrl;
+    document.body.appendChild(iframe);
+
+    iframe.contentWindow?.focus();
+    iframe.contentWindow?.print();
+
+    setTimeout(() => {
+      document.body.removeChild(iframe);
+    }, 1000); // Esperar un segundo antes de eliminar el iframe
+  }
+
+  imprimirCompromiso() {
+    const pdfUrl = 'assets/CompromisoEmprendedor.pdf'; // Reemplaza con la ruta correcta a tu archivo PDF
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = pdfUrl;
+    document.body.appendChild(iframe);
+
+    iframe.contentWindow?.focus();
+    iframe.contentWindow?.print();
+
+    setTimeout(() => {
+      document.body.removeChild(iframe);
+    }, 1000); // Esperar un segundo antes de eliminar el iframe
+  }
+  imprimirEncuesta() {
+    const pdfUrl = 'assets/EncuestaEmprendedor.pdf'; // Reemplaza con la ruta correcta a tu archivo PDF
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = pdfUrl;
+    document.body.appendChild(iframe);
+
+    iframe.contentWindow?.focus();
+    iframe.contentWindow?.print();
+
+    setTimeout(() => {
+      document.body.removeChild(iframe);
+    }, 1000); // Esperar un segundo antes de eliminar el iframe
+  }
 
 }
--- Front/crm-posta/src/app/procesos/ver-procesos/estructura-costos-modal/estructura-costos-modal.component.ts
+++ Front/crm-posta/src/app/procesos/ver-procesos/estructura-costos-modal/estructura-costos-modal.component.ts
@@ -2,6 +2,10 @@ import { Component, Input } from '@angular/core';
 import { Process } from '../../Process';
 import { ModalService } from 'src/app/client/modal.service';
 import { CostComponent } from '../../CostComponent';
+import { ProcesoService } from '../../proceso.service';
+import Swal from 'sweetalert2';
+import { CostStructure } from '../../CostStructure';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-estructura-costos-modal',
@@ -10,24 +14,78 @@ import { CostComponent } from '../../CostComponent';
 })
 export class EstructuraCostosModalComponent {
   @Input()proceso:Process= new Process();
+  @Input()bool:boolean;
+
   listaVariable:CostComponent[];
   listaFijo:CostComponent[];
+
+  listaBackend: CostComponent[] = [];
+  listaBackend2: CostComponent[] = [];
+  amount: number;
+  nameComponent: string;
+  amount1: number;
+  nameComponent1: string;
+  id: number;
+  costoEntidad: CostStructure = new CostStructure;
+  listaCostos: CostComponent[] = [];
+  nuevaEntidad: CostComponent = new CostComponent;
+  nuevaEntidad1: CostComponent = new CostComponent;
+
+
+
   constructor(
     private modalServide:ModalService,
+    private procesoService:ProcesoService,
+    private route:Router,
   ){}
 
   ngOnInit(): void {
-    console.log(this.proceso);
     this.listaVariable=this.proceso.canvasModel.costStructure.costosVariables;
-    console.log(this.listaVariable);
-    
     this.listaFijo=this.proceso.canvasModel.costStructure.costosFijos;
-    console.log(this.listaFijo); 
-    
-    
   }
 
   public cerrarVerMas(){
     this.modalServide.cerrarVerMas8();
   }
+  public editar(){
+    this.procesoService.estructuraCostoPut(this.proceso.canvasModel.costStructure).subscribe(dato=>{
+    this.modalServide.cerrarVerMas8();
+    this.route.navigate(["/procesos"])
+    Swal.fire('Editado ', 'La estructura de costos fue editada', 'success');
+    })
+  }
+
+  ver(){
+    console.log(this.proceso);
+    
+  }
+  eliminarVariable(costoComponent:CostComponent){
+    console.log(costoComponent);
+    this.proceso.canvasModel.costStructure.costosVariables= this.proceso.canvasModel.costStructure.costosVariables.filter(item => item!== costoComponent)
+      
+  }
+  eliminarFijo(costoComponent:CostComponent){
+    console.log(costoComponent);
+    this.proceso.canvasModel.costStructure.costosFijos= this.proceso.canvasModel.costStructure.costosFijos.filter(item => item!== costoComponent)
+      
+  }
+  agregarValor() {
+    this.nuevaEntidad= new CostComponent;
+     this.nuevaEntidad.amount=this.amount;
+     this.nuevaEntidad.nameComponent=this.nameComponent;
+     this.proceso.canvasModel.costStructure.costosVariables.push(this.nuevaEntidad);
+     console.log(this.proceso.canvasModel.costStructure.costosVariables);
+
+    
+
+  }
+  agregarValor1() {
+    this.nuevaEntidad1= new CostComponent;
+    this.nuevaEntidad1.amount = this.amount1;
+    this.nuevaEntidad1.nameComponent = this.nameComponent1;
+    this.proceso.canvasModel.costStructure.costosFijos.push(this.nuevaEntidad1);
+    console.log(this.proceso.canvasModel.costStructure.costosFijos);
+
+  }
+
 }
