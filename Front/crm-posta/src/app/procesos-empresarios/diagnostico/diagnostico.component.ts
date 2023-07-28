922cc3cd386906c673eb2574a4cb01f53f3a70a4
damian <adamodamian6@gmail.com>
2023-07-27 23:44:49 -0300

    consolidado en proceso empresarial 1er formulario

--- Front/crm-posta/src/app/procesos-empresarios/diagnostico/diagnostico.component.ts
+++ Front/crm-posta/src/app/procesos-empresarios/diagnostico/diagnostico.component.ts
@@ -39,10 +39,10 @@ export class DiagnosticoComponent implements OnInit {
   usuario: Usuario = new Usuario();
   constructor(private clienteService: ClientService, private processEmpresarioService: ProcessEmpresarioService, private ruta: Router, private parametro: ActivatedRoute) { }
   ngOnInit(): void {
-    this.consolidado=true;
+   /* this.consolidado=true;
     this.processEmpresarioService.procesoEmpresarioFindById(1).subscribe(data=>{
       this.procesoEmpresario=data;
-    })
+    })*/
     this.procesoEmpresario.diagnosticoEmpresarial= new DiagnosticoEmpresarial();
     this.procesoEmpresario.diagnosticoEmpresarial.diagnostico= new Diagnostico();
     //console.log(this.concepto.length);
