99ce6d2cd804a2dd830d646de73f99effafe33b6
Ulises Gadea <ulisesgadea28@gmail.com>
2023-07-31 16:48:13 -0300

    algo

--- Front/crm-posta/src/app/procesos-empresarios/resultados/resultados.component.ts
+++ Front/crm-posta/src/app/procesos-empresarios/resultados/resultados.component.ts
@@ -5,6 +5,7 @@ import { ClientService } from 'src/app/client/client.service';
 import { ProcessEmpresarioService } from '../process-empresario.service';
 import { Client } from 'src/app/client/client';
 import { lastDayOfDecade } from 'date-fns';
+import { AnalisisResultados } from '../analisis-resultados';
 
 @Component({
   selector: 'app-resultados',
@@ -26,6 +27,9 @@ this.ruta.paramMap.subscribe(parametro=>{
       this.procesos.forEach(p=>{
         if(p.client.id=this.cliente.id){
           this.procesoEmpresario= p;
+          this.procesoEmpresario.diagnosticoEmpresarial.analisisResultados=new AnalisisResultados();
+          console.log('hola');
+          
           console.log(p);
 
         }
@@ -35,4 +39,15 @@ this.ruta.paramMap.subscribe(parametro=>{
 })
   }
 
+
+  guardar(){
+    console.log(this.procesoEmpresario.diagnosticoEmpresarial.analisisResultados);
+    //this.procesoEmpresarioservice.
+    
+  }
+
+
+  guardarYcontinuar(){
+
+  }
 }
